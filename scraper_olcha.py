0832ea945e5cf28ee7a95f921517e13fb617b388
hbncompany <129648961+hbncompany@users.noreply.github.com>
2026-05-30 11:16:39 +0500

    Update scraper_olcha.py

--- scraper_olcha.py
+++ scraper_olcha.py
@@ -10,105 +10,105 @@ search_text,
 group_id
 ):
 
-results = []
-
-url = (
-    f"https://olcha.uz/oz/search"
-    f"?q={quote(search_text)}"
-)
-
-headers = {
-
-    "User-Agent":
-    "Mozilla/5.0"
-
-}
-
-response = requests.get(
-    url,
-    headers=headers,
-    timeout=30
-)
-
-soup = BeautifulSoup(
-    response.text,
-    "html.parser"
-)
-
-products = soup.select(
-    "div.product-card"
-)
-
-for product in products:
-
-    try:
-
-        name_el = product.select_one(
-            ".product-card__brand-name"
-        )
-
-        price_el = product.select_one(
-            ".price__main"
-        )
-
-        link_el = product.select_one(
-            "a.product-card__link"
-        )
-
-        img_el = product.select_one(
-            ".product-card__image img[src]"
-        )
-
-        if not (
-            name_el and
-            price_el and
-            link_el
-        ):
-            continue
-
-        name = name_el.get_text(
-            strip=True
-        )
-
-        price = int(
-
-            re.sub(
-                r"\D",
-                "",
-                price_el.text
+    results = []
+    
+    url = (
+        f"https://olcha.uz/oz/search"
+        f"?q={quote(search_text)}"
+    )
+    
+    headers = {
+    
+        "User-Agent":
+        "Mozilla/5.0"
+    
+    }
+    
+    response = requests.get(
+        url,
+        headers=headers,
+        timeout=30
+    )
+    
+    soup = BeautifulSoup(
+        response.text,
+        "html.parser"
+    )
+    
+    products = soup.select(
+        "div.product-card"
+    )
+    
+    for product in products:
+    
+        try:
+    
+            name_el = product.select_one(
+                ".product-card__brand-name"
             )
-
-        )
-
-        image_url = ""
-
-        if img_el:
-
-            image_url = img_el.get(
-                "src",
-                ""
+    
+            price_el = product.select_one(
+                ".price__main"
             )
-
-        results.append({
-
-            "group_id": group_id,
-
-            "store_name": "Olcha",
-
-            "product_name": name,
-
-            "image_url": image_url,
-
-            "product_url":
-            "https://olcha.uz"
-            + link_el["href"],
-
-            "price": price
-
-        })
-
-    except:
-
-        pass
-
-return results
+    
+            link_el = product.select_one(
+                "a.product-card__link"
+            )
+    
+            img_el = product.select_one(
+                ".product-card__image img[src]"
+            )
+    
+            if not (
+                name_el and
+                price_el and
+                link_el
+            ):
+                continue
+    
+            name = name_el.get_text(
+                strip=True
+            )
+    
+            price = int(
+    
+                re.sub(
+                    r"\D",
+                    "",
+                    price_el.text
+                )
+    
+            )
+    
+            image_url = ""
+    
+            if img_el:
+    
+                image_url = img_el.get(
+                    "src",
+                    ""
+                )
+    
+            results.append({
+    
+                "group_id": group_id,
+    
+                "store_name": "Olcha",
+    
+                "product_name": name,
+    
+                "image_url": image_url,
+    
+                "product_url":
+                "https://olcha.uz"
+                + link_el["href"],
+    
+                "price": price
+    
+            })
+    
+        except:
+    
+            pass
+    
+    return results
