7546046c8b3e15affd362e57280259b23c4ae715
hbncompany <129648961+hbncompany@users.noreply.github.com>
2026-05-30 15:06:15 +0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -4,52 +4,37 @@ import sys
 from scraper_asaxiy import scrape_asaxiy
 from scraper_texnomart import scrape_texnomart
 
-group_id = int(sys.argv[1])
+if len(sys.argv) < 4:
 
-group_name = sys.argv[2]
+    group_id = 2
+    group_name = "konditsioner"
+    search_text = "artel 12"
 
-search_text = sys.argv[3]
+else:
 
-# Guruh + foydalanuvchi matni
+    group_id = int(sys.argv[1])
+    group_name = sys.argv[2]
+    search_text = sys.argv[3]
+
 query = f"{search_text} {group_name}"
+
+print("QUERY:", query)
 
 all_products = []
 
-# try:
-
-#     products = scrape_asaxiy(
-#         query,
-#         group_id
-#     )
-
-#     all_products.extend(
-#         products
-#     )
-
-# except Exception as e:
-
-#     print(
-#         "ASAXIY ERROR:",
-#         e
-#     )
-
-try:
-
-    products = scrape_texnomart(
+all_products.extend(
+    scrape_asaxiy(
         query,
         group_id
     )
+)
 
-    all_products.extend(
-        products
+all_products.extend(
+    scrape_texnomart(
+        query,
+        group_id
     )
-
-except Exception as e:
-
-    print(
-        "TEXNOMART ERROR:",
-        e
-    )
+)
 
 with open(
     "products.json",
@@ -63,7 +48,3 @@ with open(
         ensure_ascii=False,
         indent=2
     )
-
-print(
-    f"Saved {len(all_products)} products"
-)
