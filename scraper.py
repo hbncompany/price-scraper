7a130c844471c51d5f1d1eeac639bc1afc7dfc81
hbncompany <129648961+hbncompany@users.noreply.github.com>
2026-05-30 14:10:11 +0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -27,52 +27,52 @@ all_products = []
 
 for group in groups:
 
-group_id = group["id"]
-
-keywords = [
-    x.strip()
-    for x in group["search_keywords"].split(",")
-    if x.strip()
-]
-
-print(
-    f"GROUP: {group['subgroup_name']}"
-)
-
-for keyword in keywords:
-
-    try:
-
-        print(
-            f"SEARCH: {keyword}"
-        )
-
-        products = scrape_asaxiy(
-            keyword,
-            group_id
-        )
-
-        all_products.extend(
-            products
-        )
-
-    except Exception as e:
-
-        print(e)
-
-with open(
-"products.json",
-"w",
-encoding="utf-8"
-) as f:
-
-json.dump(
-    all_products,
-    f,
-    ensure_ascii=False,
-    indent=2
-)
-
-print(
-f"Saved {len(all_products)} products"
-)
+    group_id = group["id"]
+    
+    keywords = [
+        x.strip()
+        for x in group["search_keywords"].split(",")
+        if x.strip()
+    ]
+    
+    print(
+        f"GROUP: {group['subgroup_name']}"
+    )
+    
+    for keyword in keywords:
+    
+        try:
+    
+            print(
+                f"SEARCH: {keyword}"
+            )
+    
+            products = scrape_asaxiy(
+                keyword,
+                group_id
+            )
+    
+            all_products.extend(
+                products
+            )
+    
+        except Exception as e:
+    
+            print(e)
+    
+    with open(
+    "products.json",
+    "w",
+    encoding="utf-8"
+    ) as f:
+    
+    json.dump(
+        all_products,
+        f,
+        ensure_ascii=False,
+        indent=2
+    )
+    
+    print(
+    f"Saved {len(all_products)} products"
+    )
