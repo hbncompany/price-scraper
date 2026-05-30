caf6a2557ec82e8eed6ee60e4599dd363835e5dc
hbncompany <129648961+hbncompany@users.noreply.github.com>
2026-05-30 14:52:53 +0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,85 +1,26 @@
 import json
-import mysql.connector
+import sys
 
 from scraper_asaxiy import scrape_asaxiy
-from scraper_texnomart import scrape_texnomart
 
-db = mysql.connector.connect(
-host="hbncompany.mysql.pythonanywhere-services.com",
-user="hbncompany",
-password="Sersarson7",
-database="hbncompany$default"
+group_id = int(sys.argv[1])
+
+search_text = sys.argv[2]
+
+products = scrape_asaxiy(
+    search_text,
+    group_id
 )
 
-cursor = db.cursor(dictionary=True)
-
-cursor.execute("""
-SELECT
-id,
-subgroup_name,
-search_keywords
-FROM product_groups
-WHERE is_active=1
-""")
-
-groups = cursor.fetchall()
-
-all_products = []
-
-for group in groups:
-    group_id = group["id"]
-    
-    keywords = [
-        x.strip()
-        for x in group["search_keywords"].split(",")
-        if x.strip()
-    ]
-    
-    print(
-        f"GROUP: {group['subgroup_name']}"
-    )
-    
-    for keyword in keywords:
-    
-        try:
-    
-            print(
-                f"SEARCH: {keyword}"
-            )
-    
-            # products = scrape_asaxiy(
-            #     keyword,
-            #     group_id
-            # )
-            # all_products.extend(
-            #     products
-            # )
-            
-            products = scrape_texnomart(
-                keyword,
-                group_id
-            )
-            
-            all_products.extend(
-                data
-            )
-    
-        except Exception as e:
-    
-            print(e)
-    
-    with open(
+with open(
     "products.json",
     "w",
     encoding="utf-8"
-    ) as f:
-        json.dump(
-            all_products,
-            f,
-            ensure_ascii=False,
-            indent=2
-        )
-        
-        print(
-        f"Saved {len(all_products)} products"
-        )
+) as f:
+
+    json.dump(
+        products,
+        f,
+        ensure_ascii=False,
+        indent=2
+    )
